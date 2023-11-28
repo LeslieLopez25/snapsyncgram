e26372e47cfc1ac83134b55186c44b4c98fe629e
Leslie Lopez <leslie2515lopez@gmail.com>
2023-11-27 23:47:50 -0600

    Setting up appwrite for sign up component

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -15,6 +15,7 @@ import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
   const isLoading = false;
@@ -32,7 +33,7 @@ const SignupForm = () => {
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // const newUser = await createUserAccount(value)
+    const newUser = await createUserAccount(values);
   }
 
   return (

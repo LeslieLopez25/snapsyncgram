5f573027fd9611b91af66050d2ef6e0f4376b439
Leslie Lopez <leslie2515lopez@gmail.com>
2023-12-10 23:12:59 -0600

    Debugging sign up form

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -2,6 +2,7 @@ import { ID } from "appwrite";
 
 import { INewUser } from "@/types";
 import { account, appwriteConfig, avatars, databases } from "./config";
+import { Query } from "@tanstack/react-query";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -57,6 +58,26 @@ export async function signInAccount(user: { email: string; password: string }) {
     const session = await account.createEmailSession(user.email, user.password);
 
     return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await account.get();
+
+    if (!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
   } catch (error) {
     console.log(error);
   }

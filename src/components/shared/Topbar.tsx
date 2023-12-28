b2563dd06ce4befe3674e14cf08c17ee91c1c2fd
Leslie Lopez <leslie2515lopez@gmail.com>
2023-12-28 01:16:20 -0600

    Fixing issue with formatting in global.css

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -5,8 +5,8 @@ import { useEffect } from "react";
 import { useUserContext } from "@/context/AuthContext";
 
 const Topbar = () => {
-  const { mutate: signOut, isSuccess } = useSignOutAccount;
-  const navigate = useNavigate;
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
   const { user } = useUserContext();
 
   useEffect(() => {
@@ -35,7 +35,7 @@ const Topbar = () => {
           </Button>
           <Link to={`/profile/${user.id}`} className="flex-center gap-3">
             <img
-              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
               alt="profile"
               className="h-8 w-8 rounded-full"
             />

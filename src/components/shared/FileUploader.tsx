7cab89cb35ca4f656bc8fad1bf94a385bff6ed96
Leslie Lopez <leslie2515lopez@gmail.com>
2024-01-05 23:57:33 -0600

    Setting up the FileUploader component

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,14 +1,28 @@
 import React, { useCallback, useState } from "react";
-import { useDropzone } from "react-dropzone";
+import { FileWithPath, useDropzone } from "react-dropzone";
+import { Button } from "../ui/button";
 
-const FileUploader = () => {
+type FileUploaderProps = {
+  fieldChange: (FILES: File[]) => void;
+  mediaUrl: string;
+};
+
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+  const [file, setFile] = useState([]);
   const [fileUrl, setFileUrl] = useState("");
 
-  const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath) => {
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+    },
+    [file]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
     onDrop,
+    accept: { "image/*": [".png", ".jpeg", ".jpg", ".svg"] },
   });
 
   return (
@@ -17,7 +31,25 @@ const FileUploader = () => {
       className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer"
     >
       <input {...getInputProps()} className="cursor-pointer" />
-      {fileUrl ? <div>test 1</div> : <div>test 2</div>}
+      {fileUrl ? (
+        <div>test 1</div>
+      ) : (
+        <div className="file_uploader-box">
+          <img
+            src="/assets/icons/file-upload.svg"
+            width={96}
+            height={77}
+            alt="file-upload"
+          />
+
+          <h3 className="base-medium text-light-2 mb-2 mt-6">
+            Drag photo here
+          </h3>
+          <p>SVG, PNG, JPG</p>
+
+          <Button className="shad-button_dark_4">Select from computer</Button>
+        </div>
+      )}
     </div>
   );
 };

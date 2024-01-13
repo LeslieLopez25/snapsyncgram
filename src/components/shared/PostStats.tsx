0d9bcf4299ff6610fb55201c8f68a87cb4741f0b
Leslie Lopez <leslie2515lopez@gmail.com>
2024-01-12 23:14:33 -0600

    Adding the logic to PostStats component

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,11 @@
+import { useUserContext } from "@/context/AuthContext";
+import {
+  useDeleteSavedPost,
+  useLikePost,
+  useSavePost,
+} from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
+import { useState } from "react";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -6,6 +13,21 @@ type PostStatsProps = {
 };
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
+  const likesList = post.likes.map((user: Models.Document) => user.$id);
+
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { mutate: likePost } = useLikePost();
+  const { mutate: savePost } = useSavePost();
+  const { mutate: deleteSavedPost } = useDeleteSavedPost();
+
+  const { data: currentUser } = useUserContext();
+
+  const handleLikePost = () => {};
+
+  const handleSavePost = () => {};
+
   return (
     <div className="flex justify-between items-center z-20">
       <div className="flex gap-2 mr-5">
@@ -17,7 +39,18 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
           onClick={() => {}}
           className="cursor-pointer"
         />
-        <p>0</p>
+        <p className="small-medium lg:base-medium">0</p>
+      </div>
+
+      <div className="flex gap-2">
+        <img
+          src="/assets/icons/save.svg"
+          alt="like"
+          width={20}
+          height={20}
+          onClick={() => {}}
+          className="cursor-pointer"
+        />
       </div>
     </div>
   );

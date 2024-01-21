67c9beccbeb44c552bf4d42f2d3bec98a2cb8d6f
Leslie Lopez <leslie2515lopez@gmail.com>
2024-01-20 23:53:27 -0600

    Setting up the explore component

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+          <img
+            src="/assets/icons/search.svg"
+            alt="search"
+            width={24}
+            height={24}
+          />
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Explore;

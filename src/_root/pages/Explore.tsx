0563e4a2d6b54fbe5345e27a65825beb14790c7d
Leslie Lopez <leslie2515lopez@gmail.com>
2024-01-21 23:21:04 -0600

    Finished setting up GridPostList component

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -7,7 +7,7 @@ const Explore = () => {
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
-        <h2 className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
           <img
             src="/assets/icons/search.svg"
             alt="search"
@@ -21,7 +21,7 @@ const Explore = () => {
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
           />
-        </h2>
+        </div>
       </div>
     </div>
   );
